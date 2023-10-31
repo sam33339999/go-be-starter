b6df3e98cb3bc6de688c145f7cfccc416974b96e
sam.zheng <sam.zheng@coolbe.com.tw>
2023-11-01 02:57:50 +0800

    Refactor user service and add user repository

--- domains/user_service.go
+++ domains/user_service.go
@@ -1,16 +1,13 @@
 package domains
 
-import (
-	"github.com/sam33339999/go-be-starter/models"
-	"gorm.io/gorm"
-)
+import "github.com/sam33339999/go-be-starter/models"
 
 type UserService interface {
-	WithTrx(trxHandle *gorm.DB) UserService
+	// WithTrx(trxHandle *gorm.DB) UserService
 	GetOneUser(id uint) (models.User, error)
-	GetAllUser() ([]models.User, error)
+	// GetAllUser() ([]models.User, error)
 
-	CreateUser(models.User) error
-	UpdateUser(models.User) error
-	DeleteUser(models.User) error
+	// CreateUser(models.User) error
+	// UpdateUser(models.User) error
+	// DeleteUser(models.User) error
 }
--- services/user_service.go
+++ services/user_service.go
@@ -1,1 +1,26 @@
 package services
+
+import (
+	"github.com/sam33339999/go-be-starter/domains"
+	"github.com/sam33339999/go-be-starter/lib"
+	"github.com/sam33339999/go-be-starter/models"
+)
+
+type UserService struct {
+	env    lib.Env
+	logger lib.Logger
+}
+
+func NewUserService(
+	env lib.Env,
+	logger lib.Logger,
+) domains.UserService {
+	return UserService{
+		env:    env,
+		logger: logger,
+	}
+}
+
+func (s UserService) GetOneUser(id uint) (models.User, error) {
+	return models.User{}, nil
+}
